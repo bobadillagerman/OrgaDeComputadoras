423cf1ef60357dba3bd79e1b34f6a260308af17c
Unknown <bobadillagerman@gmail.com>
2019-09-14 01:45:06 -0300

    Quito archivos por parametros

--- TP0/tp0.c
+++ TP0/tp0.c
@@ -39,11 +39,6 @@ int print_matrix(FILE* fp, matrix_t* m){
     }
     fprintf(fp, "\n");
 
-    /*if(fclose(fp)==EOF){
-        fprintf(stderr, "Error fclose: %s\n", strerror( errno ));
-        return ERROR;
-    }*/
-
     return SALIDA_EXITOSA;
 }
 
@@ -53,28 +48,20 @@ int print_matrix(FILE* fp, matrix_t* m){
 
 int main(int argc, char *argv[]) {
     int option = 0;
-    const char *short_opt = "i:o:hV";
+    const char *short_opt = "hV";
     struct option long_opt[] = {
             {"version", no_argument,       NULL, 'V'},
             {"help",    no_argument,       NULL, 'h'},
-            {"input",   required_argument, NULL, 'i'},
-            {"output",  required_argument, NULL, 'o'},
             {NULL, 0,                      NULL, 0}
     };
     FILE *inputFile = NULL;
     FILE *outputFile = NULL;
-    char ch;
-    int primerNumero = 1;
     size_t dimension;
-    //double *num;
-    double *array;
-    int posicion=0;
-    //char line[256];
     
     while ((option = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1) {
         switch (option) {
             case 'V':
-                printf("TP #0 de la materia Organizaciï¿½n de Computadoras \n");
+                printf("TP #0 de la materia Organizacion de Computadoras \n");
                 printf("Alumnos: \n");
                 printf("	Bobadilla Catalan, German\n	Leloutre, Daniela \n	Soro, Lucas \n");
                 return 0;
@@ -82,77 +69,22 @@ int main(int argc, char *argv[]) {
                 printf("Usage: \n");
                 printf("	%s -h \n", argv[0]);
                 printf("	%s -V \n", argv[0]);
-                printf("	%s [options] \n", argv[0]);
+                printf("	%s < in_file > out_file \n", argv[0]);
                 printf("Options: \n");
                 printf("	-V, --version  Print version and quit. \n");
                 printf("	-h, --help     Print this information. \n");
+		printf("Examples: \n");
+		printf("	tp0 < in.txt > out.txt \n");
+                printf("	cat in.txt | tp0 > out.txt \n");
                 return 0;
-            case 'i':
-            	if(strcmp(optarg, "-") != 0){
-					inputFile = fopen(optarg, "r");
-					if(inputFile == NULL) {
-						fprintf(stderr, "Error archivo entrada: %s\n", strerror(errno));
-						return ERROR;
-					} /*else {
-						while ((ch = fgetc(inputFile)) != EOF){
-						//while ((fscanf(inputFile,"%s",line)) != EOF){
-							//char letra;
-							//printf("Linea: %s \n", line);
-							//letra=line[0];
-							//dimension = atoi(*letra);
-							//printf("dimension: %ld", (long unsigned int)letra);
-							//dimension = (int) letra;
-							if (ch != '\n'){
-								if (primerNumero == 1){
-									//paso la dimension para crear matriz
-									dimension = (long unsigned int)ch;
-									create_matrix(dimension,dimension);
-									primerNumero = 0;
-								} else {
-									//armo la matriz
-									//double array[dimension];
-									//*array[posicion] = (double)ch;
-									//num=&array[posicion];
-									//printf("numero: %f",*array[posicion]);
-									//array[posicion]=num;
-									printf("posicion: %d \n", posicion);
-									printf("posicion: %f \n", ch);
-									//printf("%f", array[posicion]);
-									posicion++;
-								}
-							} else {
-								primerNumero = 1;
-								posicion = 0;
-							}
-							
-						}
-						printf("\n");
-						fclose(inputFile);
-					}*/
-            	}
-                break;
-            case 'o':
-            	if(strcmp(optarg, "-") != 0){
-					outputFile = fopen(optarg, "w+");
-					if(outputFile == NULL) {
-						fprintf(stderr, "Error archivo salida: %s\n", strerror(errno));
-						return ERROR;
-					}
-            	}
-                break;
             default:
-                // asï¿½ estï¿½ en el manual de getopt
+                // así está en el manual de getopt
                 abort();
         }
     }
 
-    if(inputFile == NULL) {
-        inputFile = stdin;
-    }
-
-    if(outputFile == NULL) {
-        outputFile = stdout;
-    }
+    inputFile = stdin;
+    outputFile = stdout;
 
     matrix_t matriz1;
     matrix_t matriz2;
